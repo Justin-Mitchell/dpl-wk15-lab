d2ec7957de4442934f68b7e1d42b704075ac237d
Justin Mitchell <jmitchell@Justins-MacBook-Pro.local>
2020-02-21 22:39:06 -0800

    added Game component and css resources with basic styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Game from './Game'
 import '../assets/css/app.css'
 
 function App() {
   return (
     <div className="App">
       Welcome to Week 15 LAB
+      <Game />
     </div>
   );
 }
---
+++ src/components/Game.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../assets/css/game.css'
+
+const Game = () => {
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        Insert "Board" component
+      </div>
+    </div>
+  )
+}
+
+export default Game
