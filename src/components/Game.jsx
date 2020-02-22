dec78c3fbd0a3cbcb41215f0c61783cf885446b5
Justin Mitchell <jmitchell@Justins-MacBook-Pro.local>
2020-02-21 23:07:36 -0800

    created utils folder with constants and helpers for our application

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Board from './Board'
+import { initialBoard } from '../utils/helpers'
+import { BOARD_SIZE, WALL, EMPTY, PIECE } from '../utils/constants'
 import '../assets/css/game.css'
 
 const Game = () => {
---
+++ src/utils/helpers.js
@@ -0,0 +1,11 @@
+export const initialBoard = [
+  ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
+  ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
+  ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
+  ['piece', 'piece', 'piece', 'piece', 'piece', 'piece', 'piece'],
+  ['piece', 'piece', 'piece', 'piece', 'piece', 'piece', 'piece'],
+  ['piece', 'piece', 'piece', 'piece', 'piece', 'piece', 'piece'],
+  ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
+  ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
+  ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
+]
