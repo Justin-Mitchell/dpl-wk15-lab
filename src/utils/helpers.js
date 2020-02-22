986c43b2e61849be30c2e5865847f043f67c8696
Justin Mitchell <jmitchell@Justins-MacBook-Pro.local>
2020-02-21 23:22:22 -0800

    [BUGFIX] fixed mistake in origin initialBoard matrix

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,11 +1,9 @@
 export const initialBoard = [
   ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
   ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
-  ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
   ['piece', 'piece', 'piece', 'piece', 'piece', 'piece', 'piece'],
   ['piece', 'piece', 'piece', 'piece', 'piece', 'piece', 'piece'],
   ['piece', 'piece', 'piece', 'piece', 'piece', 'piece', 'piece'],
-  ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
   ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
   ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
 ]
