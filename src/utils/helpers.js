4576cecd3fc3ce2f1afb276332ceeeb17442ffc3
Justin Mitchell <jmitchell@Justins-MacBook-Pro.local>
2020-02-21 23:23:47 -0800

    added empty center piece to initialBoard matrix

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,7 +2,7 @@ export const initialBoard = [
   ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
   ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
   ['piece', 'piece', 'piece', 'piece', 'piece', 'piece', 'piece'],
-  ['piece', 'piece', 'piece', 'piece', 'piece', 'piece', 'piece'],
+  ['piece', 'piece', 'piece', 'empty', 'piece', 'piece', 'piece'],
   ['piece', 'piece', 'piece', 'piece', 'piece', 'piece', 'piece'],
   ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
   ['wall', 'wall', 'piece', 'piece', 'piece', 'wall', 'wall'],
