d142d34150319900b51d7ca1d3a034e5ebb46997
Justin Mitchell <jmitchell@Justins-MacBook-Pro.local>
2020-02-21 23:11:51 -0800

    importing useState and created initial state of game for the app

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Board from './Board'
 import { initialBoard } from '../utils/helpers'
 import { BOARD_SIZE, WALL, EMPTY, PIECE } from '../utils/constants'
 import '../assets/css/game.css'
 
 const Game = () => {
+  const [history, setHistory] = useState([{ squares: initialBoard }])
+  const [stepNumber, setStepNumber] = useState(0)
+  const [selectedSquares, setSelectedSquares] = useState([-1,-1])
+  
   return (
     <div className='game'>
       <div className='game-board'>
