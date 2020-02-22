070bc0b0c7690c46f95091a2b47f2f5378a4f0ec
Justin Mitchell <jmitchell@Justins-MacBook-Pro.local>
2020-02-21 22:49:57 -0800

    Board component and basic styles added

---
+++ src/components/Board.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/css/board.css'
+
+const Board = () => {
+  return (
+    <div className="board">
+      ~ GAME BOARD ~
+    </div>
+  )
+}
+
+export default Board
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Board from './Board'
 import '../assets/css/game.css'
 
 const Game = () => {
   return (
     <div className='game'>
       <div className='game-board'>
-        Insert "Board" component
+        <Board />
       </div>
     </div>
   )
